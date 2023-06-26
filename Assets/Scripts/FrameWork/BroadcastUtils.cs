a5c594dff63d3271de224f45591c2f16da2c7a0f
denglang <denglang@bytedance.com>
2023-06-26 17:43:18 +0800

    feat: 添加broadcast测试样例
    Change-Id: I0ac308dd788fbeec05db12669c985fb986744e30

--- Assets/Scripts/FrameWork/BroadcastUtils.cs
+++ Assets/Scripts/FrameWork/BroadcastUtils.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace FrameWork {
 	
 	public struct TestData {
@@ -8,5 +10,32 @@ namespace FrameWork {
 	public static class BroadcastUtils {
 		public static readonly Broadcast<TestData> broadcast = new Broadcast<TestData>();
 	}
-	
+
+
+	public class TestBroadcast {
+		public void Test() {
+			
+			BroadcastUtils.broadcast.On(this.OnTestMessage);
+        
+			BroadcastUtils.broadcast.On(this.OnTestMessage2);
+        
+			Debug.Log(BroadcastUtils.broadcast.Has(this.OnTestMessage));
+        
+			BroadcastUtils.broadcast.Emit(new TestData() {testName = "this is test name"});
+        
+			BroadcastUtils.broadcast.Off(this.OnTestMessage);
+        
+			BroadcastUtils.broadcast.Emit(new TestData() {testName = "this is test name"});
+			
+		}
+		
+		
+		private void OnTestMessage(TestData t) {
+			Debug.Log("OnTestMessage: " + t.testName);
+		}
+
+		private void OnTestMessage2(TestData t) {
+			Debug.Log("OnTestMessage2: " + t.testName);   
+		}
+	}
 }
